5ecf82707e67f2881673d89de2816456f75cdd98
Dyeizon <dyeizonque@gmail.com>
2023-01-23 21:04:42 -0300

    header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap';
+import './header.css';
+
+export const Header = (props) => {
+    return (
+        <Container>
+            <header>
+                <nav>
+                    <p className='header-name'>Dyeizon Procopiuk</p>
+                    <ul className='header-list'>
+                        <li className='header-list-item'><button>About me</button></li>
+                        <li className='header-list-item'><button>Projects</button></li>
+                        <li className='header-list-item'><button>Contacts</button></li>
+                    </ul>
+                </nav>
+            </header>
+        </Container>
+    );
+}
