b4a71974864c17b473b99bc9d984b16b1e1dab33
Dyeizon <34202471+Dyeizon@users.noreply.github.com>
2023-03-21 16:45:53 -0300

    changed name from tarot page to arcannus

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,6 @@
 import './style.css';
 import reactexercisesimage from '../../assets/project-image-reactexercises.png';
-import tarotpageimage from '../../assets/project-image-tarotpage.png';
+import arcannus from '../../assets/project-image-tarotpage.png';
 
 import { Card } from './Card';
 
@@ -9,7 +9,7 @@ export const Projects = () => {
         <div className='container'>
             <div className="projects">
                 <div className="cards">
-                    <Card src={tarotpageimage} title="Tarot Page" siteHref="https://dyeizon-tarotpage.netlify.app" githubHref="https://github.com/Dyeizon/TarotPage"/>
+                    <Card src={arcannus} title="Arcannus" siteHref="https://dyeizon-arcannus.netlify.app" githubHref="https://github.com/Dyeizon/arcannus"/>
                     <Card src={reactexercisesimage} title="React Exercises" siteHref="https://dyeizon-reactexercises.netlify.app/" githubHref="https://github.com/Dyeizon/react-exercises"/>
                     <Card title="Coming Soon" soon/>
                 </div>
