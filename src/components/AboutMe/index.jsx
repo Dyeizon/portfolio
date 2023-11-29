feec581ab8cdd4ae05f990b44905cf026a6e6918
dyeizon <dyeizonque@gmail.com>
2023-11-29 14:17:45 -0300

    add vitis

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,7 +1,24 @@
 import './style.css';
 import aboutmephoto from './../../assets/aboutme-photo.png';
+import { useEffect, useState } from 'react';
+
 
 export const AboutMe = () => {
+    const [age, setAge] = useState();
+
+    var currentDate = new Date();
+    var birth = {dia: 16, mes: 7, ano: 2003}
+
+    useEffect(() => {
+        if(currentDate.getMonth() + 1 < birth['mes']) {
+            setAge(currentDate.getFullYear() - birth['ano'] - 1);
+        } else {
+            setAge(currentDate.getFullYear() - birth['ano']);
+        }
+    }, [])
+
+    
+
     return (
         <div className="container">
             <section className='aboutme'>
@@ -11,7 +28,7 @@ export const AboutMe = () => {
                 
                 <div className='aboutme-info'>
                     <p>First of all, thank you for visiting my portfolio, I hope you liked <span className='pl-1'>:)</span></p>
-                    <p>My full name is <b>Dyeizon Procopiuk da Silva</b>, and I am 19 years old. As you may notice, I am not fluent in English, I am from Brazil, so I speak Portuguese.</p>
+                    <p>My full name is <b>Dyeizon Procopiuk da Silva</b>, and I am {age} years old. As you may notice, I am not fluent in English, I am from Brazil, so I speak Portuguese.</p>
                     <p>About my passions, I really like spending my free time studying Tarology, or playing League of Legends with some friends.</p>
                     
                     <div className='aboutme-buttons mt-4'>
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,7 @@
 import './style.css';
 import reactexercisesimage from '../../assets/project-image-reactexercises.png';
 import arcannus from '../../assets/project-image-tarotpage.png';
+import vitis from '../../assets/project-image-vitis.png';
 
 import { Card } from './Card';
 
@@ -10,6 +11,7 @@ export const Projects = () => {
             <h1 className='projects-title text-center'>My Projects</h1>
             <div className="projects">
                 <div className="cards">
+                <Card src={vitis} title="Vitis" siteHref="https://vitis.vercel.app" githubHref="https://github.com/Dyeizon/vitis"/>
                     <Card src={arcannus} title="Arcannus" siteHref="https://dyeizon-arcannus.netlify.app" githubHref="https://github.com/Dyeizon/arcannus"/>
                     <Card src={reactexercisesimage} title="React Exercises" siteHref="https://dyeizon-reactexercises.netlify.app/" githubHref="https://github.com/Dyeizon/react-exercises"/>
                     <Card title="Coming Soon" soon/>
